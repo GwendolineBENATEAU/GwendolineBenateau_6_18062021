1a63a2901f5d45788993b58d9feabe71336e4cd9
Gwendoline <jouflette69@gmail.com>
2021-06-22 15:01:16 +0200

    :construction: server.js improvements

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,67 @@ const http = require('http');
 const app = require('./app');
 
 
+//améliorations du server.js, pour le rendre plus stable et approprié pour le déploiement :
+//la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
+const normalizePort = val => 
+{
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) 
+  {
+    return val;
+  }
+  if (port >= 0) 
+  {
+    return port;
+  }
+  return false;
+};
+
 //configuration de l'écoute de l'app express
-app.set('port', process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+//la fonction errorHandler recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
+const errorHandler = error => 
+{
+  if (error.syscall !== 'listen') 
+  {
+    throw error;
+  }
+
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+
+  switch (error.code) 
+  {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+
+    default:
+      throw error;
+  }
+};
 
 //appel de la méthode createServer qui sera appellée à chaque execution du serveur
 const server = http.createServer(app);
 
-//configuration de l'écoute du server
-server.listen(process.env.PORT || 3000);
+//
+server.on('error', errorHandler);
+server.on('listening', () => 
+{
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  // écouteur d'évènements consignant le port ou le canal sur lequel le serveur s'exécute
+  console.log('Listening on ' + bind);
+});
 
+//configuration de l'écoute du server
+server.listen(port);
