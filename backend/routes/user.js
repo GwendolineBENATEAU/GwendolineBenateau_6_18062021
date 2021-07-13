8f7ffc89f22e7da3f521ee962c2cec06ae90d8f5
Gwendoline <jouflette69@gmail.com>
2021-07-13 12:53:43 +0200

    :bug: Fix for add express-rate-limit to prevent brute-forcing attacks

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,9 +8,18 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 
 
+const rateLimit = require("express-rate-limit");
+//4 possibilite de se connecter, toutes les 15 min
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // limite de 5 tentatives
+    message:
+      "Tentatives de connexion trop nombreuse, veuillez réessayer dans 15 min"
+  });
+
 //application des controleurs dans les routes d'authentification correspondantes
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', limiter, userCtrl.login);
 
 
 
